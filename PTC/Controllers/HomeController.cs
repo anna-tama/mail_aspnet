3c4f4db8117df1f241f390a14ccd8dd27659bf9d
anna_tama <anna.tama.roma@gmail.com>
2022-12-17 11:48:44 -0300

    init: página web con envío de correo funcionando

---
+++ PTC/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PTC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PTC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Index(string to, string subject, string body)
+        {
+            try
+            {
+                Thread email = new(delegate ()
+                {
+                    SendMail.Email(to, subject, body);
+                });
+                email.IsBackground = true;
+                email.Start();
+                TempData["alert"] = "email enviado";
+            }
+            catch (Exception ex)
+            {
+                TempData["alert"] = "Problem Sending email";
+            }
+            return Redirect("Home/Index");
+        }
+
+
+
+    }
+}
---
+++ PTC/SendMail.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace PTC
+{
+    public class SendMail
+    {
+        public static void Email(string to, string subject, string body)
+        {
+            MailMessage mm = new();
+            mm.To.Add(new MailAddress(to));
+            mm.From = new MailAddress("anna.tama.roma@gmail.com");
+            mm.Subject = subject;   
+            mm.Body = body;
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            SmtpClient smtp = new ();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;    
+            smtp.EnableSsl = true;
+            smtp.Credentials = new NetworkCredential("anna.tama.roma@gmail.com", "npmsaikjjdfputqa");
+            smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            smtp.Send(mm);
+
+        }
+
+    }
+}
+ 
